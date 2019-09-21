ec3df00175532dca46eb67e8c145a899bc90394a
peterMcP <36275626+peterMcP@users.noreply.github.com>
2019-09-21 22:08:52 +0200

    added mathgeolib test and sources to solution

--- Engine/ModuleSceneIntro.cpp
+++ Engine/ModuleSceneIntro.cpp
@@ -8,9 +8,9 @@
 #include "external/MathGeoLib/include/MathGeoLib.h"
 
 #ifdef _DEBUG
-#pragma comment (lib, "external/MathGeoLib/libx86/Debug/MathGeoLib.lib") 
+#pragma comment (lib, "external/MathGeoLib/libx86/_Debug/MathGeoLib.lib") 
 #else
-#pragma comment (lib, "external/MathGeoLib/libx86/Release/MathGeoLib.lib") 
+#pragma comment (lib, "external/MathGeoLib/libx86/_Release/MathGeoLib.lib") 
 #endif
 
 
@@ -50,29 +50,49 @@ update_status ModuleSceneIntro::Update(float dt)
 	p.axis = true;
 	p.Render();
 
-	math::Sphere s1;
-	s1.r = 100;
-	//s1.pos.Set(0.0f, 0.0f, 0.0f);
+	// mathgeolib test ------------------------------------------
 
-	// mathgeolib test
-	//Sphere s1(float3(0.0f, 0.0f, 0.0f), 100.0f);
+	static float x, y, z;
+
+	Sphere s1(float3(0.0f, 0.0f, 0.0f), 50.0f);
 	//s1.r = 100.0f;
 	//s1.pos.Set(0.0f, 0.0f, 0.0f);
 
 	Sphere s2;
-	s2.r = 100.0f;
-	//s2.pos.Set(0.f, 0.f, 0.f);
 
-	/*if (s1.Intersects(s2))
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	{
+		x -= 0.5f;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	{
+		x += 0.5f;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+	{
+		z += 0.5f;
+	}
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	{
+		z -= 0.5f;
+	}
+
+	s2.r = 50.0f + z;
+	s2.pos.Set(x, 0.f, 0.0f);
+
+	LOG("S2 pos: %f,%f,%f", s2.pos.x, s2.pos.y, s2.pos.z);
+	LOG("S2 radius: %f", s2.r);
+
+	if (s1.Intersects(s2))
 	{
 		LOG("Spheres intersecting");
 	}
 	else
 	{
 		LOG("Spheres not intersecting");
-	}*/
+	}
 	
-
+	// ----------------------------------------------------------
 	
 
 	return UPDATE_CONTINUE;
