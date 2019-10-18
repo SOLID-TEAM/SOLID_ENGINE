3dbd625ae6207646a80f9e1c05e666a3c4214134
peterMcP <36275626+peterMcP@users.noreply.github.com>
2019-10-18 17:16:41 +0200

    cleaned up modeldata and moduleimporter

--- Engine/ModelData.cpp
+++ Engine/ModelData.cpp
@@ -28,19 +28,12 @@ ModelData::ModelData(float* vertices, uint* indices, float* normals, float* uvs,
 	_v_size = nVertices;
 	_idx_size = nIndices * 3;
 
-	GenerateBuffers();
-	/*ComputeVertexNormals();
-	ComputeFacesNormals();*/
-	UpdateBuffers();
+	Start();
 }
 
-bool ModelData::Start(float v_n_line_length, float f_n_line_length)
+bool ModelData::Start()
 {
 	GenerateBuffers();
-
-	//ComputeVertexNormals(v_n_line_length); // for debug draw purposes | BEFORE UPDATE BUFFERS!! to compute debugdraw normals before we fill the buffers
-	//ComputeFacesNormals(f_n_line_length);
-
 	UpdateBuffers();
 
 	return true;
@@ -61,12 +54,7 @@ bool ModelData::GenerateBuffers()
 	glGenBuffers(1, &normals_gl_id);
 	if(uvs != nullptr)
 		glGenBuffers(1, &uv_gl_id);
-	//// debug draw vertices normals (vertices already stored and ready to be used for this in normal way)
-	//glGenBuffers(1, &debug_v_normals_gl_id);
-	//// debug draw faces normals buffers
-	//glGenBuffers(1, &debug_f_vertices_gl_id);
-	//glGenBuffers(1, &debug_f_normals_gl_id);
-
+	
 	// TODO: move to moduletextures
 	// texture buffer
 	/*glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -98,47 +86,7 @@ bool ModelData::UpdateBuffers()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indices_gl_id);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint) * _idx_size, &indices[0], GL_STATIC_DRAW);
 
-	//// WARNING: FOR NORMALS DEBUG ONLY, the normals for other calculations remains on normals float pointer ----
-	//glBindBuffer(GL_ARRAY_BUFFER, debug_v_normals_gl_id);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * _v_size, &debug_v_normals[0], GL_STATIC_DRAW);
-
-	//// TODO: 2nd warning, be careful, since we only load and print triangulated faces this is good
-	//glBindBuffer(GL_ARRAY_BUFFER, debug_f_vertices_gl_id);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(float) * _idx_size, &debug_f_vertices[0], GL_STATIC_DRAW);
-	//glBindBuffer(GL_ARRAY_BUFFER, debug_f_normals_gl_id);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(float) * _idx_size * 2, &debug_f_normals[0], GL_STATIC_DRAW);
-
-
 	//texture_gl_id = App->textures->GenerateCheckerTexture(512,512);
-
-	return ret;
-}
-
-bool ModelData::RefillDebugVertexNormalsBuffers()
-{
-	bool ret = true;
-
-	//glDeleteBuffers(1, &debug_v_normals_gl_id);
-
-
-	//glBindBuffer(GL_ARRAY_BUFFER, debug_v_normals_gl_id);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * _v_size, &debug_v_normals[0], GL_STATIC_DRAW);
-
-	return ret;
-}
-
-bool ModelData::RefillDebugFacesNormalsBuffers()
-{
-	bool ret = true;
-
-	/*glDeleteBuffers(1, &debug_f_vertices_gl_id);
-	glDeleteBuffers(1, &debug_f_normals_gl_id);*/
-	
-	
-	/*glBindBuffer(GL_ARRAY_BUFFER, debug_f_vertices_gl_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * _idx_size, &debug_f_vertices[0], GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, debug_f_normals_gl_id);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * _idx_size * 2, &debug_f_normals[0], GL_STATIC_DRAW);*/
 
 	return ret;
 }
@@ -205,6 +153,7 @@ bool ModelData::Render()
 	return ret;
 }
 
+
 bool ModelData::RenderWireframe() // need very few operations
 {
 	bool ret = true;
@@ -230,89 +179,6 @@ bool ModelData::RenderOutline()
 	return true;
 }
 
-//bool ModelData::DebugRenderVertex(float pointSize)
-//{
-//	bool ret = true;
-//
-//	// draw points
-//	glPointSize(pointSize);
-//
-//	glEnableClientState(GL_VERTEX_ARRAY);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, vertices_gl_id);
-//	glVertexPointer(3, GL_FLOAT, 0, (void*)0);
-//
-//	glDrawArrays(GL_POINTS, 0, _v_size);
-//
-//	glDisableClientState(GL_VERTEX_ARRAY);
-//
-//	glPointSize(1.0f);
-//
-//	return ret;
-//}
-//
-//bool ModelData::DebugRenderVertexNormals(float lineWidth)
-//{
-//	bool ret = true;
-//	
-//	glLineWidth(lineWidth);
-//
-//	// draw normals lines
-//	glEnableClientState(GL_VERTEX_ARRAY);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, debug_v_normals_gl_id);
-//	glVertexPointer(3, GL_FLOAT, 0, (void*)0);
-//
-//	glDrawArrays(GL_LINES, 0, _v_size * 2);
-//
-//	glDisableClientState(GL_VERTEX_ARRAY);
-//
-//	glLineWidth(1.0f);
-//
-//	return ret;
-//}
-//
-//bool ModelData::DebugRenderFacesVertex(float pointSize)
-//{
-//	bool ret = true;
-//
-//	glPointSize(pointSize);
-//
-//	// draw points
-//	glEnableClientState(GL_VERTEX_ARRAY);
-//
-//	glBindBuffer(GL_ARRAY_BUFFER, debug_f_vertices_gl_id);
-//	glVertexPointer(3, GL_FLOAT, 0, (void*)0);
-//
-//	glDrawArrays(GL_POINTS, 0, _idx_size);
-//
-//	glDisableClientState(GL_VERTEX_ARRAY);
-//
-//	glPointSize(1.0f);
-//
-//	return ret;
-//}
-//
-//bool ModelData::DebugRenderFacesNormals(float lineWidth)
-//{
-//	bool ret = true;
-//
-//	glLineWidth(lineWidth);
-//
-//	glEnableClientState(GL_VERTEX_ARRAY);
-//	
-//	// draw lines
-//	glBindBuffer(GL_ARRAY_BUFFER, debug_f_normals_gl_id);
-//	glVertexPointer(3, GL_FLOAT, 0, (void*)0);
-//
-//	glDrawArrays(GL_LINES, 0, _idx_size * 2);
-//
-//	glDisableClientState(GL_VERTEX_ARRAY);
-//
-//	glLineWidth(1.0f); // returns to default line width
-//
-//	return ret;
-//}
 
 bool ModelData::CleanUp()
 {
@@ -322,9 +188,6 @@ bool ModelData::CleanUp()
 	glDeleteBuffers(1, &indices_gl_id);
 	glDeleteBuffers(1, &uv_gl_id);
 	glDeleteBuffers(1, &normals_gl_id);
-	/*glDeleteBuffers(1, &debug_v_normals_gl_id);
-	glDeleteBuffers(1, &debug_f_vertices_gl_id);
-	glDeleteBuffers(1, &debug_f_normals_gl_id);*/
 
 	// delete all stored data
 
@@ -332,93 +195,10 @@ bool ModelData::CleanUp()
 	delete[] indices;
 	delete[] uvs;
 	delete[] normals;
-	/*delete[] debug_v_normals;
-	delete[] debug_f_vertices;
-	delete[] debug_f_normals;*/
-	
 
 	return ret;
 }
 
-//void ModelData::ComputeVertexNormals(float length)
-//{
-//	// to draw lines, we need an array ready to what expects gldrawarrays
-//	// start point and finish point
-//	// TODO: improve this, thinking in deepth if this is possible with memcpy
-//
-//	if (debug_v_normals != nullptr) delete[] debug_v_normals;
-//
-//	debug_v_normals = new float[_v_size * 6]; // 3 for startpoint, 3 more for endpoint
-//
-//	uint n_count = 0;
-//	for(uint i = 0; i < _v_size * 3; i += 3)
-//	{
-//		debug_v_normals[n_count]     = vertices[i]; // x
-//		debug_v_normals[n_count + 1] = vertices[i+1]; // y
-//		debug_v_normals[n_count + 2] = vertices[i+2]; //z
-//
-//		debug_v_normals[n_count + 3] = vertices[i] + normals[i] * length; // x
-//		debug_v_normals[n_count + 4] = vertices[i+1] + normals[i+1] * length; // y
-//		debug_v_normals[n_count + 5] = vertices[i+2] + normals[i+2] * length; // z
-//
-//		n_count += 6;
-//	}
-//
-//}
-//
-//bool ModelData::ComputeFacesNormals(float length)
-//{
-//
-//	if (_idx_size % 3 != 0)
-//	{
-//		LOG("[Error] Could not compute face normals, faces with != 3 vertex");
-//		return false;
-//	}
-//
-//	if (debug_f_vertices != nullptr) delete[] debug_f_vertices;
-//	if (debug_f_normals != nullptr) delete[] debug_f_normals;
-//
-//	debug_f_vertices = new float[_idx_size];
-//	debug_f_normals = new float[_idx_size * 2];
-//
-//	uint i = 0;
-//	uint n_count = 0;
-//	for (; i < _idx_size; i += 3)
-//	{
-//		// find the midface averaged vertex
-//
-//		float* v1 = &vertices[indices[i] * 3];     // get first face vertex
-//		float* v2 = &vertices[indices[i + 1] * 3]; // get second face vertex
-//		float* v3 = &vertices[indices[i + 2] * 3]; // get third face vertex
-//
-//		debug_f_vertices[i]     = (v1[0] + v2[0] + v3[0]) / 3.0f; // x coord
-//		debug_f_vertices[i + 1] = (v1[1] + v2[1] + v3[1]) / 3.0f; // y coord
-//		debug_f_vertices[i + 2] = (v1[2] + v2[2] + v3[2]) / 3.0f; // z coord
-//
-//		// compute the averaged normal of the 3 vertex of each face
-//
-//		float* n1 = &normals[indices[i] * 3];     // get first face vertex normal
-//		float* n2 = &normals[indices[i + 1] * 3]; // get second face vertex normal
-//		float* n3 = &normals[indices[i + 2] * 3]; // get third face vertex normal
-//
-//		float avg_n[3];
-//		avg_n[0] = (n1[0] + n2[0] + n3[0]) / 3.0f; // x coord
-//		avg_n[1] = (n1[1] + n2[1] + n3[1]) / 3.0f; // y coord
-//		avg_n[2] = (n1[2] + n2[2] + n3[2]) / 3.0f; // z coord
-//
-//		debug_f_normals[n_count]     = debug_f_vertices[i]; // x
-//		debug_f_normals[n_count + 1] = debug_f_vertices[i + 1]; // y
-//		debug_f_normals[n_count + 2] = debug_f_vertices[i + 2]; //z
-//
-//		debug_f_normals[n_count + 3] = debug_f_vertices[i] + avg_n[0] * length; // x
-//		debug_f_normals[n_count + 4] = debug_f_vertices[i + 1] + avg_n[1] * length; // y
-//		debug_f_normals[n_count + 5] = debug_f_vertices[i + 2] + avg_n[2] * length; // z
-//
-//		n_count += 6;
-//	}
-//
-//	return true;
-//}
 
 //const char* ModelData::GetName() const
 //{
--- Engine/ModelData.h
+++ Engine/ModelData.h
@@ -18,30 +18,9 @@ public:
 
 	bool GenerateBuffers(); // generate all needed buffers at once
 	bool UpdateBuffers(); // update/fill/refill all buffers
-	//UpdateVertexBuffer(uint vb_id);
-	//UpdateIndicesBuffer(uint fb_id);
-	//UpdateTexture2DBuffer(uint tex_id);
-	//UpdateTexture2DUVBuffer(uint uvs_id);
-	//UseShaderID(uint shader_program_id); 
-
-	/*bool DebugRenderVertex(float pointSize = 5.0f);
-	bool DebugRenderVertexNormals(float lineWidth = 1.0f);
-	bool DebugRenderFacesVertex(float pointSize = 5.0f);
-	bool DebugRenderFacesNormals(float lineWidth = 1.0f);
-
-	void ComputeVertexNormals(float length = 0.4f);
-	bool ComputeFacesNormals(float length = 0.4f);*/
-
-	bool RefillDebugVertexNormalsBuffers();
-	bool RefillDebugFacesNormalsBuffers();
-
-	// void Scale();
-	// void Translate();
-	// void ApplyRotation();
-	// mat4x4 GetTransform();
-
+	
 	//const char* GetName() const;
-	bool Start(float, float);
+	bool Start();
 	bool Render();
 	bool RenderWireframe();
 	bool RenderOutline();
@@ -55,52 +34,22 @@ public:
 	// model data ---------------
 	uint _v_size = 0; // vertex
 	uint _idx_size = 0; // indices
-	//uint _uv_size = 0; // texture coords
 	uint _uv_num_components = 0;
-	//uint _n_size = -1; // normals || not needed, assume each vertes has its normal
-	//uint _f_size = -1; // triangulated faces
 	float* vertices = nullptr;
 	uint* indices = nullptr;
 	float* uvs = nullptr;
 	float* normals = nullptr;
-	// debug draw normals data ------
-	//float* debug_v_normals = nullptr;
-	//// maybe we dont need this for nothing more than debug ----
-	//float* debug_f_vertices = nullptr; // this array should store 1 computed vertex(for draw the point on midface) for each face
-	//float* debug_f_normals = nullptr; // and 3 startpoint and 3 endpoint of each face
 	// --------------------------
 	// gl buffer ----------------
 	uint vertices_gl_id = 0;
 	uint indices_gl_id = 0;
-	
 	uint uv_gl_id = 0;
 	uint normals_gl_id = 0;
-	//// DEBUG DRAW PURPOSES BUFFERS ids -------------------------------
-	//uint debug_v_normals_gl_id = 0;
-	//// for debug draw faces points and line normals
-	//// TODO: implement this with stride in one float array (debug_f_vertices and debug_f_normals float arrays)
-	//uint debug_f_vertices_gl_id = 0;
-	//uint debug_f_normals_gl_id = 0;
 	// ---------------------------------------------------------------
-	uint texture_gl_id = 0;
-	//
-	float color[3] = { 1.0f,0.0f,1.0f };
-	// --------------------------
-	// world --------------------
-	//mat4x4 transform;
-	//vec4 position;
-	//vec4 direction;
-
-	//// TODO: testing here, print points and lines for vertex and faces normals debug draw
-	//bool debug_vertex_normals = true;
-	//bool debug_faces_normals = false;
-
-private:
-	// containers
-	/*std::vector<uint*> c_gl_buffers;
-	std::vector<uint*> c_sizes;
-	std::vector<uint*> c_uvs;
-	std::vector<uint*> c_normals;*/
+	// TODO: MOVE TO MATERIAL, when finished
+	uint texture_gl_id = 0; 
+	float color[3] = { 1.0f,0.0f,1.0f }; 
+	// ---------------------------------------------------------------
 	
 };
 
