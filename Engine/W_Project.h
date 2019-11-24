73d5a6f6588637dc25928898948e8b89274bf4a2
alejandro61299 <36158509+alejandro61299@users.noreply.github.com>
2019-11-24 03:43:05 +0100

    Added window project

---
+++ Engine/W_Project.h
@@ -0,0 +1,16 @@
+#ifndef _W_PROJECT_H__
+#define _W_PROJECT_H__
+
+#include "Window.h"
+
+class W_Project : public Window
+{
+	W_Project(std::string name, bool active) : Window(name, active) {};
+
+
+};
+
+
+
+#endif // !_W_PROJECT_H__
+
