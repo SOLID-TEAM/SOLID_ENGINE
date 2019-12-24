c363786b920521640f3b77cbcde65072c2f6b5c0
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2019-12-24 20:29:38 +0100

    Fixed rotation and added trigger options

--- Engine/C_Collider.cpp
+++ Engine/C_Collider.cpp
@@ -47,11 +47,21 @@ bool C_Collider::Update()
 
 	if (body == nullptr || shape == nullptr) return true;
 
+	// Static logic -----------------------------------------
+
+	if (linked_go->is_static == true && body->getMass() != 0.f)
+	{
+		btVector3 innertia;
+		shape->calculateLocalInertia(0.f, innertia);
+		body->setMassProps(0.f, innertia);
+	}
+
 	// Match Size Scalling ----------------------------------
 
 	float3 scaled_size = size.Mul( linked_go->transform->scale.Abs());
 	float3 scaled_center = center.Mul(linked_go->transform->scale);
 
+	scaled_size = CheckInvalidCollider(scaled_size);
 	shape->setLocalScaling(btVector3(scaled_size.x, scaled_size.y, scaled_size.z));
 
 	// Set Transform ------------------------------------------
@@ -61,9 +71,10 @@ bool C_Collider::Update()
 		btTransform transform = body->getCenterOfMassTransform();
 		btVector3 position = transform.getOrigin();
 		btMatrix3x3 rotation = transform.getBasis();
+		btQuaternion quat_rotation = transform.getRotation();
 	
 		linked_go->transform->SetPosition(float3(position - rotation * ToBtVector3(scaled_center)));
-		//linked_go->transform->SetRotation(math::Quat((btScalar*)rotation));
+		linked_go->transform->SetRotation(math::Quat((btScalar*)quat_rotation));
 	}
 	else
 	{
@@ -79,63 +90,41 @@ bool C_Collider::Update()
 	return true;
 }
 
-bool C_Collider::Render()
+bool C_Collider::DrawPanelInfo()
 {
-	//if (App->scene->selected_go != linked_go) 
-	//	return true;
+	bool last_is_trigger = is_trigger;
 
-	if (body == nullptr || shape == nullptr) return true;;
+	ImGui::Spacing();
+	ImGui::Title("Is Trigger", 1);	ImGui::Checkbox("##is_trigger", &last_is_trigger);
+	ImGui::Title("Center", 1);	ImGui::DragFloat3("##center", center.ptr(), 0.1f);
+	ImGui::Title("Size", 1);	ImGui::DragFloat3("##size", size.ptr(), 0.1f, 0.01f);
+	ImGui::Spacing();
 
-	uint num_edges = shape->getNumEdges();
-	btVector3 a, b;
-	btTransform transform = body->getCenterOfMassTransform();
-	GLfloat ptr[16];
-	transform.getOpenGLMatrix(ptr);
-
-	glPushMatrix();
-	glMultMatrixf(ptr);
-	ModuleRenderer3D::BeginDebugDraw(float4(0.f, 1.f, 0.f, 1.f));
-	glBegin(GL_LINES);
-
-	for (uint i = 0; i < num_edges; ++i)
+	if (last_is_trigger != is_trigger)
 	{
-		shape->getEdge(i, a, b);
-		glVertex3fv((btScalar*)a);
-		glVertex3fv((btScalar*)b);
+		SetIsTrigger(last_is_trigger);
 	}
 
-	glEnd();
-	ModuleRenderer3D::EndDebugDraw();
-	glPopMatrix();
-
-	glPointSize(4.f);
-
-	ModuleRenderer3D::BeginDebugDraw(float4(1.f, 0.f, 0.f, 1.f));
-	glBegin(GL_POINTS);
-	glVertex3fv(&linked_go->transform->position[0]);
-	glEnd();
-	ModuleRenderer3D::EndDebugDraw();
-
-	glPointSize(7.f);
-
-	ModuleRenderer3D::BeginDebugDraw(float4(0.f, 0.f, 1.f, 1.f));
-	glBegin(GL_POINTS);
-	btVector3 c = body->getCenterOfMassPosition();
-	glVertex3fv(&c[0]);
-	glEnd();
-	ModuleRenderer3D::EndDebugDraw();
-	
 	return true;
 }
 
-bool C_Collider::DrawPanelInfo()
+void C_Collider::SetIsTrigger(bool value)
 {
-	ImGui::Spacing();
-	ImGui::Title("Center", 1);	ImGui::DragFloat3("##center", center.ptr(), 0.1f);
-	ImGui::Title("Size", 1);	ImGui::DragFloat3("##size", size.ptr(), 0.1f);
-	ImGui::Spacing();
+	if (value == true)
+	{
+		body->setCollisionFlags(body->getCollisionFlags() | btCollisionObject::CF_NO_CONTACT_RESPONSE);
+	}
+	else
+	{
+		body->setCollisionFlags(body->getCollisionFlags() & ~btCollisionObject::CF_NO_CONTACT_RESPONSE);
+	}
 
-	return true;
+	is_trigger = value;
+}
+
+bool C_Collider::GetIsTrigger()
+{
+	return is_trigger;
 }
 
 
@@ -175,4 +164,58 @@ void C_Collider::LoadCollider()
 	body->setUserPointer(linked_go);
 
 	App->physics->AddBody(body);
+}
+
+float3 C_Collider::CheckInvalidCollider(float3 size)
+{
+	return size.Max(float3(0.01, 0.01, 0.01));
+}
+
+bool C_Collider::Render()
+{
+	//if (App->scene->selected_go != linked_go) 
+	//	return true;
+
+	if (body == nullptr || shape == nullptr) return true;;
+
+	uint num_edges = shape->getNumEdges();
+	btVector3 a, b;
+	btTransform transform = body->getCenterOfMassTransform();
+	GLfloat ptr[16];
+	transform.getOpenGLMatrix(ptr);
+
+	glPushMatrix();
+	glMultMatrixf(ptr);
+	ModuleRenderer3D::BeginDebugDraw(float4(0.f, 1.f, 0.f, 1.f));
+	glBegin(GL_LINES);
+
+	for (uint i = 0; i < num_edges; ++i)
+	{
+		shape->getEdge(i, a, b);
+		glVertex3fv((btScalar*)a);
+		glVertex3fv((btScalar*)b);
+	}
+
+	glEnd();
+	ModuleRenderer3D::EndDebugDraw();
+	glPopMatrix();
+
+	//glPointSize(4.f);
+
+	//ModuleRenderer3D::BeginDebugDraw(float4(1.f, 0.f, 0.f, 1.f));
+	//glBegin(GL_POINTS);
+	//glVertex3fv(&linked_go->transform->position[0]);
+	//glEnd();
+	//ModuleRenderer3D::EndDebugDraw();
+
+	//glPointSize(7.f);
+
+	//ModuleRenderer3D::BeginDebugDraw(float4(0.f, 0.f, 1.f, 1.f));
+	//glBegin(GL_POINTS);
+	//btVector3 c = body->getCenterOfMassPosition();
+	//glVertex3fv(&c[0]);
+	//glEnd();
+	//ModuleRenderer3D::EndDebugDraw();
+
+	return true;
 }
